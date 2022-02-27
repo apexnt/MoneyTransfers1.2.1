2cd9295fde60607c02a49462053bb0595db26fa7
apexnt <apexnt@yandex.ru>
2022-02-27 22:22:31 +0500

    correction IDE

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,15 @@
+import javax.swing.JOptionPane
+
 fun main() {
     val amount = 25500000
     val minimumCommission = 3500
     val transferFee = 75
-    val commission: Int
     val rub: Int
     val kopek: Int
 
-    if ((amount * transferFee / 10000) <= minimumCommission) {
-        commission = minimumCommission
-    } else {
-        commission = amount * transferFee / 10000
-    }
+    val commission: Int = if ((amount * transferFee / 10000) <= minimumCommission) minimumCommission
+    else amount * transferFee / 10000
+
     rub = commission / 100
     kopek = commission - (rub * 100)
         println("Комиссия за перевод: " + rub + "руб. " + kopek + " коп.")
