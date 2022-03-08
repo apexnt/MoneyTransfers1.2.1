34a367e9df1bd13f29a732bfa08db0c5b6dda350
apexnt <apexnt@yandex.ru>
2022-03-08 15:31:04 +0500

    rework

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,20 @@
-const val MINIMUM_COMMISSION = 35_00U
-const val TRANSFER_FEE = 75U
+const val MINIMUM_COMMISSION = 35_00.0
+const val TRANSFER_FEE = (0.75 / 100)
 
 fun main() {
+
     println("Добро пажаловать в ''переводы ВКонтакте''")
     println("Комиссия за перевод 0.75% от суммы перевода, минимум 35 руб.")
     print("Введите сумму перевода: ")
-    val amount = readLine()?.toUInt() ?: return
-    val amountKop = amount * 100u
-    val commission: UInt = if ((amountKop * TRANSFER_FEE / 10_000u) <= MINIMUM_COMMISSION) MINIMUM_COMMISSION
-    else amountKop * TRANSFER_FEE / 10_000u
-    val outputRub = (commission / 100u).toInt()
-    val outputKopek = (commission - ((outputRub * 100).toUInt())).toInt()
-    print("Комиссия за перевод: $outputRub руб. $outputKopek коп.")
+    val amount = readLine()?.toInt() ?: return
+    val amountKop = amount * 100
+    var commission = amountKop * TRANSFER_FEE
+    commission = if (commission <= MINIMUM_COMMISSION) {
+        MINIMUM_COMMISSION
+    } else amountKop * TRANSFER_FEE
+    val outputRub = (commission / 100 ).toInt()
+    val outputKopek = (commission %100).toInt()
+    print("Комиссия за перевод $outputRub руб. $outputKopek коп.")
 }
 
 
