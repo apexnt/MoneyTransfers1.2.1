9fd31dc04289c76f9f6a5c83f27bdedeb48d86e6
apexnt <apexnt@yandex.ru>
2022-03-22 20:16:09 +0500

    add cheking Limits

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,14 @@ const val MINIMUM_COMMISSION_VISA_MIR = 35_00.0
 const val TRANSFER_FEE_VISA_MIR = (0.75 / 100)
 const val TRANSFER_FEE_MASTERCARD_MAESTRO = (0.6 / 100)
 const val LIMIT_MASTERCARD_MAESTRO = 75_000_00.0
+const val MAX_AMOUNT_TRANSFER_DAY_CARDS = 150_000_00
+const val MAX_AMOUNT_TRANSFER_MONTH_CARDS = 600_000_00
+const val MAX_AMOUNT_TRANSFER_VKPAY = 15_000_00
+const val MAX_AMOUNT_TRANSFER_MONTH_VKPAY = 40_000_00
 
 fun main() {
     println("Добро пажаловать в \"переводы ВКонтакте\"")
-    println("Выберите тип карты/счета используемых для перевода:")
+    print("Выберите тип карты/счета используемых для перевода:")
     print("Для Master Card и Maestro введиете - 1, для Visa и Мир - 2, для VK Pay - 3: ")
     val paymentCardType = readLine()?.toInt() ?: return
     val outPaymentCardType = transferConditionsTypeOfCard(paymentCardType)
@@ -19,7 +23,13 @@ fun main() {
     val amountTransferKop = amountTransfer * 100
     val amountOfPastTransfersKop = amountOfPastTransfers * 100
 
-    val commission = transferFee(paymentCardType, amountOfPastTransfersKop, amountTransferKop)
+    val limits = checkingLimits(paymentCardType, amountTransferKop, amountTransferKop)
+
+    if (!limits) {
+        println("Превышен лимит на перевод. Попробуйте другой способ перевода")
+        return
+    }
+    val commission = transferFee(paymentCardType, amountOfPastTransfersKop, amountTransferKop, limits)
 
     val outCommission = transformationCommissionOfText(commission)
 
@@ -29,20 +39,35 @@ fun main() {
 fun transferConditionsTypeOfCard(paymentCardType: Int) {
     when (paymentCardType) {
         1 -> {
-            println(
-                "За переводы с карт Mastercard и Maestro комиссия не взимается при сумме перевода до 75 000 руб. " +
-                        "в календарный месяц, в иных случаях - 0,6% + 20 руб."
-            )
+            println("За переводы с карт Mastercard и Maestro комиссия не взимается при сумме перевода до 75 000 руб. ")
+            println("в календарный месяц, в иных случаях - 0,6% + 20 руб. ")
+            println("Максимальная сумма перевода 150 000 руб. в сутки и 600 000 руб. в месяц")
         }
         2 -> {
-            println("Комиссия за переводы с карт Visa и МИР - 0,75% минимум 35 руб. ")
+            println("Комиссия за переводы с карт Visa и МИР - 0,75% минимум 35 руб. Максимальная сумма перевода 150 000 руб. в сутки и 600 000 руб. в месяц")
         }
-        else -> println("Комиссия за переводы на счет VK Pay - не взимается")
+        else -> println(
+            "Комиссия за переводы на счет VK Pay - не взимается. Максимальная сумма перевода 15 000 руб. " +
+                    "за один раз и 40 000 руб. в месяц."
+        )
     }
-
 }
 
-fun transferFee(paymentCardType: Int = 3, amountOfPastTransfersKop: Int = 0, amountTransferKop: Int): Double {
+fun checkingLimits(paymentCardType: Int, amountTransferKop: Int, amountOfPastTransfersKop: Int): Boolean {
+    return when {
+        paymentCardType == 1 && amountTransferKop > MAX_AMOUNT_TRANSFER_DAY_CARDS
+                || paymentCardType == 1 && amountOfPastTransfersKop > MAX_AMOUNT_TRANSFER_MONTH_CARDS -> false
+        paymentCardType == 2 && amountTransferKop > MAX_AMOUNT_TRANSFER_DAY_CARDS
+                || paymentCardType == 2 && amountOfPastTransfersKop > MAX_AMOUNT_TRANSFER_MONTH_CARDS -> false
+        paymentCardType == 3 && amountTransferKop > MAX_AMOUNT_TRANSFER_VKPAY
+                || paymentCardType == 3 && amountOfPastTransfersKop > MAX_AMOUNT_TRANSFER_MONTH_VKPAY -> false
+        else -> {
+            true
+        }
+    }
+}
+
+fun transferFee(paymentCardType: Int = 3, amountOfPastTransfersKop: Int = 0, amountTransferKop: Int, limits: Boolean = true): Double {
     return when {
         paymentCardType == 1 && amountOfPastTransfersKop >= LIMIT_MASTERCARD_MAESTRO -> {
             amountTransferKop * TRANSFER_FEE_MASTERCARD_MAESTRO + 20_00
