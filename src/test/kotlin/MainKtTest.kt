d695b2c547e42ba4ecad7e2f79bddfcbea20f860
apexnt <apexnt@yandex.ru>
2022-03-26 22:33:50 +0500

    add test

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -36,7 +36,69 @@ class MainKtTest {
         assertEquals(false, result)
     }
 
+    @Test
+    fun checkingLimitsVisaMirTrue() {
+        //arrange
+        val paymentCardType = 2
+        val amountTransferKop = 150_000_00
+        val amountOfPastTransfersKop = 600_000_00
+        //act
+        val result = checkingLimits(
+            paymentCardType = paymentCardType,
+            amountTransferKop = amountTransferKop,
+            amountOfPastTransfersKop = amountOfPastTransfersKop
+        )
+        //assert
+        assertEquals(true, result)
+    }
 
+    @Test
+    fun checkingLimitsVisaMirFalse() {
+        //arrange
+        val paymentCardType = 2
+        val amountTransferKop = 151_000_00
+        val amountOfPastTransfersKop = 600_000_00
+        //act
+        val result = checkingLimits(
+            paymentCardType = paymentCardType,
+            amountTransferKop = amountTransferKop,
+            amountOfPastTransfersKop = amountOfPastTransfersKop
+        )
+        //assert
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun checkingLimitsVKPayTrue() {
+        //arrange
+        val paymentCardType = 3
+        val amountTransferKop = 15_000_00
+        val amountOfPastTransfersKop = 40_000_00
+        //act
+        val result = checkingLimits(
+            paymentCardType = paymentCardType,
+            amountTransferKop = amountTransferKop,
+            amountOfPastTransfersKop = amountOfPastTransfersKop
+        )
+        //assert
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun checkingLimitsVKPayFalse() {
+        //arrange
+        val paymentCardType = 3
+        val amountTransferKop = 16_000_00
+        val amountOfPastTransfersKop = 40_000_00
+        //act
+        val result = checkingLimits(
+            paymentCardType = paymentCardType,
+            amountTransferKop = amountTransferKop,
+            amountOfPastTransfersKop = amountOfPastTransfersKop
+        )
+        //assert
+        assertEquals(false, result)
+    }
 
     @Test
     fun transferFeeMastercardMaestro() {
@@ -50,6 +112,8 @@ class MainKtTest {
             amountOfPastTransfersKop = amountOfPastTransfersKop,
             amountTransferKop = amountTransferKop
         )
+
+
 
         assertEquals(8_000.0, result, 8_000.0)
     }
