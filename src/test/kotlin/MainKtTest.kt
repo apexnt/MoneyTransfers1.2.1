0a55d428d7620c9f0c114f426fe64e0e55e502cf
apexnt <apexnt@yandex.ru>
2022-03-26 22:36:30 +0500

    add test+

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -22,99 +22,118 @@ class MainKtTest {
 
     @Test
     fun checkingLimitsMastercardMaestroFalse() {
-        //arrange
         val paymentCardType = 1
         val amountTransferKop = 151_000_00
         val amountOfPastTransfersKop = 600_000_00
-        //act
         val result = checkingLimits(
             paymentCardType = paymentCardType,
             amountTransferKop = amountTransferKop,
             amountOfPastTransfersKop = amountOfPastTransfersKop
         )
-        //assert
         assertEquals(false, result)
     }
 
     @Test
     fun checkingLimitsVisaMirTrue() {
-        //arrange
         val paymentCardType = 2
         val amountTransferKop = 150_000_00
         val amountOfPastTransfersKop = 600_000_00
-        //act
         val result = checkingLimits(
             paymentCardType = paymentCardType,
             amountTransferKop = amountTransferKop,
             amountOfPastTransfersKop = amountOfPastTransfersKop
         )
-        //assert
         assertEquals(true, result)
     }
 
     @Test
     fun checkingLimitsVisaMirFalse() {
-        //arrange
         val paymentCardType = 2
         val amountTransferKop = 151_000_00
         val amountOfPastTransfersKop = 600_000_00
-        //act
         val result = checkingLimits(
             paymentCardType = paymentCardType,
             amountTransferKop = amountTransferKop,
             amountOfPastTransfersKop = amountOfPastTransfersKop
         )
-        //assert
         assertEquals(false, result)
     }
 
     @Test
     fun checkingLimitsVKPayTrue() {
-        //arrange
         val paymentCardType = 3
         val amountTransferKop = 15_000_00
         val amountOfPastTransfersKop = 40_000_00
-        //act
         val result = checkingLimits(
             paymentCardType = paymentCardType,
             amountTransferKop = amountTransferKop,
             amountOfPastTransfersKop = amountOfPastTransfersKop
         )
-        //assert
         assertEquals(true, result)
     }
 
     @Test
     fun checkingLimitsVKPayFalse() {
-        //arrange
         val paymentCardType = 3
         val amountTransferKop = 16_000_00
         val amountOfPastTransfersKop = 40_000_00
-        //act
         val result = checkingLimits(
             paymentCardType = paymentCardType,
             amountTransferKop = amountTransferKop,
             amountOfPastTransfersKop = amountOfPastTransfersKop
         )
-        //assert
         assertEquals(false, result)
     }
 
     @Test
-    fun transferFeeMastercardMaestro() {
+    fun transferFeeMastercardMaestrolimitExceeded() {
         val paymentCardType = 1
         val amountTransferKop = 10_000_00
-        val amountOfPastTransfersKop = 7500000
-
-
+        val amountOfPastTransfersKop = 75_000_00
         val result = transferFee(
             paymentCardType = paymentCardType,
             amountOfPastTransfersKop = amountOfPastTransfersKop,
             amountTransferKop = amountTransferKop
         )
-
-
-
         assertEquals(8_000.0, result, 8_000.0)
+    }
+
+    @Test
+    fun transferFeeMastercardMaestrolimitNotExceeded() {
+        val paymentCardType = 1
+        val amountTransferKop = 10_000_00
+        val amountOfPastTransfersKop = 74_000_00
+        val result = transferFee(
+            paymentCardType = paymentCardType,
+            amountOfPastTransfersKop = amountOfPastTransfersKop,
+            amountTransferKop = amountTransferKop
+        )
+        assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun transferFeeVisaMirMinCommission() {
+        val paymentCardType = 2
+        val amountTransferKop = 3_000_00
+        val amountOfPastTransfersKop = 1_000
+        val result = transferFee(
+            paymentCardType = paymentCardType,
+            amountOfPastTransfersKop = amountOfPastTransfersKop,
+            amountTransferKop = amountTransferKop
+        )
+        assertEquals(35_00.0, result, 35_00.0)
+    }
+
+    @Test
+    fun transferFeeVisaMirNotMinCommission() {
+        val paymentCardType = 2
+        val amountTransferKop = 10_000_00
+        val amountOfPastTransfersKop = 1_000
+        val result = transferFee(
+            paymentCardType = paymentCardType,
+            amountOfPastTransfersKop = amountOfPastTransfersKop,
+            amountTransferKop = amountTransferKop
+        )
+        assertEquals(75_00.0, result, 75_00.0)
     }
 }
